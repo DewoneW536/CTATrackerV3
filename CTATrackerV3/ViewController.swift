b9716e030200558564cbf3694bd077c0a1bfa271
Dewone Westerfield <dwesterf@depaul.edu>
2021-05-04 22:54:55 -0500

    Initial Commit

---
+++ CTATrackerV3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CTATrackerV3
+//
+//  Created by Dewone Westerfield on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
